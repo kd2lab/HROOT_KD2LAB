ffe7d6b2727f34db27ab18de0c71f2bef61c75fb
ingmar <mail@ingmar.net>
2011-07-14 09:46:03 +0200

    added unicorn and deploy

---
+++ config/deploy.rb
@@ -0,0 +1,33 @@
+# capistrano integration
+$:.unshift(File.expand_path('./lib', ENV['rvm_path']))
+require "rvm/capistrano"
+set :rvm_ruby_string, "1.9.2@boilerplate"
+set :use_sudo, false
+
+set :bundle_dir, ""
+set :bundle_flags, ""
+require "bundler/capistrano"
+
+set :application, "hroot"
+set :repository,  "git@ingmar.net:hroot.git"
+
+set :scm, :git
+
+set :deploy_to, "/var/www/rails/#{application}"
+set :user, "root"
+
+server "root@lvps83-169-5-139.dedicated.hosteurope.de", :app, :web, :db
+
+namespace :deploy do
+  desc "Tell unicorn to restart the app."
+  task :restart, :roles => :app do
+    run "(test -f #{current_path}/tmp/pids/unicorn.pid && kill -HUP `cat #{current_path}/tmp/pids/unicorn.pid`) || (cd #{current_path} && RAILS_ENV=qa unicorn_rails -c #{current_path}/config/unicorn.rb -D)"
+  end
+  
+  desc "Compile css"
+  task :compile, :roles => :app do
+    run "cd #{current_path} && compass compile"
+  end
+end
+
+before "deploy:restart", "deploy:compile"
---
+++ config/unicorn.rb
@@ -0,0 +1,4 @@
+worker_processes 2
+pid         "/var/www/rails/hroot/shared/pids/unicorn.pid"
+stderr_path "/var/www/rails/hroot/shared/log/unicorn.log"
+stdout_path "/var/www/rails/hroot/shared/log/unicorn.log"
