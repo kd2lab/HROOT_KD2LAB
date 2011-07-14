4c42c98f265498eb02b9198428e76c05769c9298
ingmar <mail@ingmar.net>
2011-07-14 09:58:04 +0200

    changed unicorn

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,4 +1,5 @@
 worker_processes 2
 pid         "/var/www/rails/hroot/shared/pids/unicorn.pid"
-stderr_path "/var/www/rails/hroot/shared/log/unicorn.log"
-stdout_path "/var/www/rails/hroot/shared/log/unicorn.log"
+#stderr_path "/var/www/rails/hroot/shared/log/unicorn.log"
+#stdout_path "/var/www/rails/hroot/shared/log/unicorn.log"
+listen 8081
