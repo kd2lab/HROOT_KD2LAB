f9dfb0ea27707e2e4c1a5f9f3f2f8598d981e77b
ingmar <mail@ingmar.net>
2011-10-18 11:37:04 +0200

    locations can be active or inactive now

--- db/migrate/20110817093013_create_locations.rb
+++ db/migrate/20110817093013_create_locations.rb
@@ -3,6 +3,7 @@ class CreateLocations < ActiveRecord::Migration
     create_table :locations do |t|
       t.string :name
       t.string :description
+      t.boolean :active, :default => true
       t.timestamps
     end
   end
