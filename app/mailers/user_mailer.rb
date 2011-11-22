2dbe23a93abd6c20f7f165422c6dcfefacd076d5
ingmar <mail@ingmar.net>
2011-11-23 00:29:06 +0100

    change mail

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,17 +2,17 @@ class UserMailer < ActionMailer::Base
   default from: "hroot@ingmar.net"
   
   def welcome_email
-    mail(:to => "mail@ingmar.net", :subject => "Hello")
+    mail(:to => "ingmar.baetge@googlemail.com", :subject => "Hello")
   end
   
   def log_mail(subject, text)
     @text = text
-    mail(:to => "mail@ingmar.net", :subject => subject, :text => text)
+    mail(:to => "ingmar.baetge@googlemail.com", :subject => subject, :text => text)
   end
   
   def invitation_email(user, experiment)
     @text = experiment.invitation_text_for(user)
     from = if experiment.sender_email.blank? then default[:from] else experiment.sender_email end
-    mail(:to => "mail@ingmar.net", :subject => experiment.invitation_subject, :from => from)
+    mail(:to => "ingmar.baetge@googlemail.com", :subject => experiment.invitation_subject, :from => from)
   end
 end
