1a91f51b558238a8212a128cf0c32c509475c3f9
ingmar <mail@ingmar.net>
2012-06-28 13:09:08 +0200

    more asset stuff

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,10 +29,10 @@ Hroot::Application.configure do
 
   # Disable Rails's static asset server
   # In production, Apache or nginx will already do this
-  # config.serve_static_assets = false
+  config.serve_static_assets = false
 
   # Enable serving of images, stylesheets, and javascripts from an asset server
-  # config.action_controller.asset_host = "http://assets.example.com"
+  config.action_controller.asset_host = "https://www.wiso.uni-hamburg.de/hroot/assets"
 
   # Disable delivery errors, bad email addresses will be ignored
   config.action_mailer.delivery_method = :sendmail
