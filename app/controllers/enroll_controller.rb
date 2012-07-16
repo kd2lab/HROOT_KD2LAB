29125954aa99a18ab1ede44d91a4a01f127d706d
ingmar <mail@ingmar.net>
2012-07-16 13:10:43 +0200

    bigfixes

--- app/controllers/enroll_controller.rb
+++ app/controllers/enroll_controller.rb
@@ -56,17 +56,17 @@ EOSQL
       subject = @session.experiment.confirmation_subject.to_s.mreplace({
         "#firstname" => current_user.firstname, 
         "#lastname"  => current_user.lastname,
-        "#session_date"  => @session.start_at.strftime("%d.%m.%Y"),
-        "#session_start_time" => @session.start_at.strftime("%H:%M"),
-        "#session_end_time" => @session.end_at.strftime("%H:%M")
+        # "#session_date"  => @session.start_at.strftime("%d.%m.%Y"),
+        # "#session_start_time" => @session.start_at.strftime("%H:%M"),
+        # "#session_end_time" => @session.end_at.strftime("%H:%M")
       })
       
       text = @session.experiment.confirmation_text.to_s.mreplace({
         "#firstname" => current_user.firstname, 
         "#lastname"  => current_user.lastname,
-        "#session_date"  => @session.start_at.strftime("%d.%m.%Y"),
-        "#session_start_time" => @session.start_at.strftime("%H:%M"),
-        "#session_end_time" => @session.end_at.strftime("%H:%M"),
+        # "#session_date"  => @session.start_at.strftime("%d.%m.%Y"),
+        # "#session_start_time" => @session.start_at.strftime("%H:%M"),
+        # "#session_end_time" => @session.end_at.strftime("%H:%M"),
         "#sessionlist"  =>  ([@session] + @session.following_sessions).map{|s| s.start_at.strftime("%d.%m.%Y, %H:%M Uhr")+(if s.location then " (Ort: #{s.location.name.chomp})" else "" end) }.join("\n")
       })
       
