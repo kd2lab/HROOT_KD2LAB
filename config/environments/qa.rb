37f8e1c64ee8aeeaa344201c1d96a0ecbcd81d63
ingmar <mail@ingmar.net>
2012-08-08 12:08:25 +0200

    asset?

--- config/environments/qa.rb
+++ config/environments/qa.rb
@@ -62,7 +62,7 @@ Hroot::Application.configure do
     :host => 'www.wiso.uni-hamburg.de/hroot'
   }
   
-  #config.assets.prefix = "assets"
+  config.assets.prefix = "assettest"
   config.path_prefix = nil
 end
 
