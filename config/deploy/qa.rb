93db43e5e0298f4fd0be43859daecff5eccfc39a
ingmar <mail@ingmar.net>
2012-12-13 13:36:16 +0100

    deployment stuff

--- config/deploy/qa.rb
+++ config/deploy/qa.rb
@@ -2,9 +2,10 @@ set :deploy_to, "/var/www/rails/#{application}"
 set :user, "root"
 server "root@lvps83-169-5-139.dedicated.hosteurope.de", :app, :web, :db, :primary => true
 set :rails_env, "qa"
-set :rvm_path, "/usr/local/bin/rvm" 
+# set :rvm_path, "/usr/local/bin/rvm" 
+# set :rvm_bin_path, "/usr/local/bin"
 
 # unicorn integration
-set :unicorn_binary, "/usr/local/rvm/gems/ruby-1.9.2-p0@hroot/bin/unicorn"
+set :unicorn_binary, "/usr/local/rvm/gems/ruby-1.9.2-p320@hroot/bin/unicorn"
 set :unicorn_config, "#{current_path}/config/unicorn.rb"
 set :unicorn_pid, "#{current_path}/tmp/pids/unicorn.pid"
