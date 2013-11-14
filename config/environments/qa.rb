b558585b687922b4523d312e6a133c8354aa8bde
n0c1urne <mail@ingmar.net>
2013-11-14 23:14:49 +0100

    assets

--- config/environments/qa.rb
+++ config/environments/qa.rb
@@ -29,8 +29,8 @@ Hroot::Application.configure do
 
   # Disable Rails's static asset server
   # In production, Apache or nginx will already do this
-  config.serve_static_assets = true
-  config.assets.debug = true
+  # config.serve_static_assets = true
+  # config.assets.debug = true
   
   
   # Enable serving of images, stylesheets, and javascripts from an asset server
