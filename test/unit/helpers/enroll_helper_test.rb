b78ffa9a15e5a67325b5858e0d90b9f934b7f0ea
n0c1urne <mail@ingmar.net>
2013-11-19 13:21:24 +0100

    removed a lot of files

--- test/unit/helpers/enroll_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class EnrollHelperTest < ActionView::TestCase
-end
--- test/unit/helpers/user_sessions_controller_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class UserSessionsControllerHelperTest < ActionView::TestCase
-end
