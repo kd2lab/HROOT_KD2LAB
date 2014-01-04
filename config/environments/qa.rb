fc50cec077551b2707a54583595272b0528a3e8a
n0c1urne <mail@ingmar.net>
2014-01-04 17:48:15 +0100

    mail on qa

--- config/environments/qa.rb
+++ config/environments/qa.rb
@@ -40,12 +40,12 @@ Hroot::Application.configure do
   #config.action_mailer.delivery_method = :sendmail
   #config.action_mailer.perform_deliveries = true
   
-  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.delivery_method = :sendmail
   config.action_mailer.perform_deliveries = true
-  #config.action_mailer.smtp_settings = {
-  #  :address              => "localhost",
-  #  :port                 => "25"
-  #}
+  config.action_mailer.smtp_settings = {
+    :address              => "localhost",
+    :port                 => "25"
+  }
   config.action_mailer.raise_delivery_errors = true
  
 
