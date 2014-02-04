420882b82202757c61dae0506f168bf0f455d772
n0c1urne <mail@ingmar.net>
2014-02-04 13:27:51 -0500

    Reverting one more commit (should have been in feature branch)
    Revert "initial commit of group model"
    
    This reverts commit 2fadc4f7cf1f59bb9cd39ced34aacc4bb7eeadac.

--- app/models/session_group.rb
+++
@@ -1,3 +0,0 @@
-class SessionGroup < ActiveRecord::Base
-  attr_accessible :id, :signup_mode
-end
--- test/factories/session_groups.rb
+++
@@ -1,8 +0,0 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
-FactoryGirl.define do
-  factory :session_group do
-    id 1
-    signup_mode 1
-  end
-end
