e5dd6cc17ae5def5000b11fe51bda3f1335b7301
n0c1urne <mail@ingmar.net>
2014-03-06 14:32:05 +0100

    changes to sessiongroup and constants, refactored view

--- app/models/session_group.rb
+++ app/models/session_group.rb
@@ -1,8 +1,7 @@
 class SessionGroup < ActiveRecord::Base
-  USER_VISITS_ONE_SESSION_OF_GROUP = 1
+  USER_IS_RANDOMIZED_TO_ONE_SESSION = 1
   USER_VISITS_ALL_SESSIONS_OF_GROUP = 2
-  USER_IS_RANDOMIZED_TO_ONE_SESSION = 3
-
+  
   belongs_to :experiment
   has_many :sessions, :dependent => :nullify
 
